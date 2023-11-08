5aaf3afe0e96317d56386493b091f21481fcb327
anbehindY <yanminthwin@gmail.com>
2023-11-08 23:52:46 +0630

    Update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,67 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundClip: {
-        'text': 'text',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'right-bg-gradient': 'url(/bg-gradient-rt.svg)',
-        'left-bg-gradient': 'url(/bg-gradient-lt.svg)',
-      },
-      backgroundPosition: {
-        'right': 'right 75%',
-      },
-      boxShadow: {
-        'listen-btn': '0 1px 38px 0 rgba(190, 12, 12, 0.82)',
-        'down-arrow': '0 0 34px 2px rgba(255, 33, 80, 0.7)',
-      },
-    },
-  },
-  variants: {
-    extend: {
-      backgroundClip: ['responsive'],
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {        
+			backgroundClip: {
+				text: 'text',
+			},
+			backgroundImage: {
+				'right-bg-gradient': 'url(/bg-gradient-rt.svg)',
+				'left-bg-gradient': 'url(/bg-gradient-lt.svg)',
+				'lay-phyu': 'url(/layphyu.svg)',
+				'big-bag': 'url(/bigbag.svg)',
+				'eternal-gosh': 'url(/eternalgosh.svg)',
+				'r-zarni': 'url(/RZarni.svg)',
+			},
+			backgroundPosition: {
+				right: 'right 75%',
+				top: 'bottom center%',
+			},
+			boxShadow: {
+				'listen-btn': '0 1px 38px 0 rgba(190, 12, 12, 0.82)',
+				'down-arrow': '0 0 34px 0 rgba(255, 33, 80, 0.7)',
+				'artist-img': '0 0.2px 7px 0 rgba(238, 64, 64, 0.66)',
+			},
+			listStyleImage: {
+				'tick': 'url(/tickSquare.svg)',
+				'cross': 'url(/crossSquare.svg)',
+			},
+			listStylePosition: {
+				inside: 'inside',
+			},
+		},
+	},
+	variants: {
+		extend: {
+			backgroundClip: ['responsive'],
+		},
+	},
+	plugins: [
+		function({ addUtilities }) {
+      const newUtilities = {
+        '.list-style-tick': {
+          'background-image': "url('/tickSquare.svg')",
+          'background-repeat': 'no-repeat',
+          'background-position': 'left center',
+          'padding': '3px 0px 3px 10px',
+          'list-style': 'none',
+          'margin': '0',
+        },
+				'.list-style-cross': {
+          'background-image': "url('/crossSquare.svg')",
+          'background-repeat': 'no-repeat',
+          'background-position': 'left center',
+          'padding': '3px 0px 3px 10px',
+          'list-style': 'none',
+          'margin': '0',
+        },
+      }
+      addUtilities(newUtilities)
+    }
+	],
+};
