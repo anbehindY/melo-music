d68c3665b372cf3899ac0357b0182a425873492f
anbehindY <yanminthwin@gmail.com>
2023-11-15 02:56:14 +0630

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,26 +5,26 @@ import { FaFacebookF, FaTiktok } from 'react-icons/fa';
 const Footer = () => {
 	return (
 		<footer>
-			<div className='flex flex-col gap-8 items-center justify-center text-center pt-8 py-10 border-b-[1px] border-[#5B5858] w-[90%] mx-auto'>
-				<ul className='text-[16px] flex flex-col gap-2 leading-5'>
+			<div className='flex flex-col gap-8 items-center justify-center text-center pt-8 py-10 border-b-[1px] border-[#5B5858] w-[90%] mx-auto lg:flex-row lg:justify-between'>
+				<ul className='text-[16px] flex flex-col gap-2 leading-5 lg:items-start'>
 					<li className='font-bold !leading-7'>Information</li>
 					<li>FAQ</li>
 					<li>Blog</li>
 					<li>Support</li>
 				</ul>
-				<ul className='text-[16px] flex flex-col gap-2 leading-5'>
+				<ul className='text-[16px] flex flex-col gap-2 leading-5 lg:items-start'>
 					<li className='font-bold !leading-7'>Melo</li>
 					<li>About Us</li>
 					<li>Career</li>
 					<li>Contact Us</li>
 				</ul>
-				<ul className='text-[16px] flex flex-col gap-2 leading-5'>
+				<ul className='text-[16px] flex flex-col gap-2 leading-5 lg:items-start'>
 					<li className='font-bold !leading-7'>Legal</li>
 					<li>Terms and Conditions</li>
 					<li>Privacy Policy</li>
 					<li>Terms of Use</li>
 				</ul>
-				<ul className='text-left text-sm leading-4 flex flex-col gap-8 items-start w-[95%]'>
+				<ul className='text-left text-sm leading-4 flex flex-col gap-8 items-start w-[95%] lg:w-[380px] '>
 					<li className='flex gap-6 items-start leading-4'>
 						<Image
 							src={'/location.svg'}
@@ -57,7 +57,7 @@ const Footer = () => {
 					</li>
 				</ul>
 			</div>
-			<div className='flex flex-col pt-8 pb-8 gap-6'>
+			<div className='flex flex-col py-8 gap-6 lg:flex-row-reverse lg:justify-between lg:mx-[5%] lg:py-0 lg:h-[95px]'>
 				<ul className='flex justify-center items-center gap-6 text-xl text-white'>
 					<li>
 						<a
@@ -84,7 +84,14 @@ const Footer = () => {
 					</li>
 				</ul>
 				<div className='flex gap-6 items-center justify-center'>
-					<Image src={'/Logo.svg'} alt='logo' width={93} height={35} />
+					<div className='w-[93px] h-[35px] lg:w-[124px] lg:h-[47px] relative'>
+						<Image
+							src={'/Logo.svg'}
+							alt='logo'
+							fill
+							style={{ objectFit: 'contain' }}
+						/>
+					</div>
 					<p className='leading-[22px]'>
 						{' '}
 						@2023<span className='gradient'> MID</span>. All rights served.
