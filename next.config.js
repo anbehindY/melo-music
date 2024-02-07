fc872bb9d4bfa681d030a0669d5f40f78317a569
anbehindY <yanminthwin@gmail.com>
2024-02-07 16:18:20 +0630

    Configure utils for api integration

--- next.config.js
+++ next.config.js
@@ -3,16 +3,20 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**play.google.com',
+        protocol: "https",
+        hostname: "**play.google.com",
       },
       {
-        protocol: 'https',
-        hostname: 'tools.applemediaservices.com',
+        protocol: "https",
+        hostname: "tools.applemediaservices.com",
       },
     ],
     unoptimized: true,
   },
-}
+  env: {
+    API_URL: process.env.API_URL,
+    API_KEY: process.env.API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/utils/endpoints.js
@@ -0,0 +1,16 @@
+const prefix = "/api/mmn";
+
+export const endpoints = Object.freeze({
+  privacyPolicy: {
+    get: `${prefix}/privacy-policy`,
+  },
+  termsAndConditions: {
+    get: `${prefix}/terms-and-conditions`,
+  },
+  termsOfUse: {
+    get: `${prefix}/terms-of-use`,
+  },
+  support: {
+    get: `${prefix}/support-guide`,
+  },
+});
