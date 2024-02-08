2ba07891fcab1ce48119a63b319bc4d2617719e1
anbehindY <yanminthwin@gmail.com>
2024-02-08 22:34:58 +0630

    Update generateMetadata function

--- src/app/page.js
+++ src/app/page.js
@@ -6,32 +6,39 @@ import { endpoints } from "@/utils/endpoints";
 import axios from "axios";
 
 export async function generateMetadata({ searchParams }) {
-  try {
-    const ogData = await axios
-      .post(
-        `${process.env.API_URL}${endpoints.ogData.get}?id=${searchParams.id}&type=${searchParams.type}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "api-key": process.env.API_KEY,
-          },
-        }
-      )
-      .then((res) => res.data);
+  if (searchParams.id && searchParams.type) {
+    try {
+      const ogData = await axios
+        .post(
+          `${process.env.API_URL}${endpoints.ogData.get}?id=${searchParams.id}&type=${searchParams.type}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+              "api-key": process.env.API_KEY,
+            },
+          }
+        )
+        .then((res) => res.data);
 
-    return {
-      title: "Melo Music",
-      description:
-        "Melo Music is a music streaming service that allows you to listen to your favorite songs and artists.",
-      openGraph: {
-        title: ogData.title,
-        description: ogData.description,
-        images: ogData.image,
-      },
-    };
-  } catch (error) {
-    console.log(error);
+      return {
+        title: "Melo Music",
+        description:
+          "Melo Music is a music streaming service that allows you to listen to your favorite songs and artists.",
+        openGraph: {
+          title: ogData.title,
+          description: ogData.description,
+          images: ogData.image,
+        },
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
+  return {
+    title: "Melo Music",
+    description:
+      "Melo Music is a music streaming service that allows you to listen to your favorite songs and artists.",
+  };
 }
 
 export default function Home() {
