4a2ae4f3c41cc42fd7b001b6e11eff421a88652e
anbehindY <yanminthwin@gmail.com>
2024-02-13 16:32:31 +0630

    Update accordium animation

--- src/components/support/AccordiumItem.jsx
+++ src/components/support/AccordiumItem.jsx
@@ -1,45 +1,97 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import { Collapse } from 'react-collapse';
-import { TbChevronRight, TbChevronDown } from 'react-icons/tb';
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { TbChevronRight, TbChevronDown } from "react-icons/tb";
 
 const AccordiumItem = ({ question, answer }) => {
-	const [dropDownStatus, setDropDownStatus] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<div
-			className={`lg:w-4/5 w-[95%] box-border border-2 border-transparent items-start mx-auto bg-white flex flex-col justify-between lg:px-10 lg:py-10 px-6 py-6 font-[500] lg:text-[22px] text-lg rounded-xl
-			${dropDownStatus && '!border-[#FF2150]'}`}
-		>
-			<div className='flex justify-between w-full items-center'>
-				<p className='w-4/5'>{question}</p>
-				<div
-					className={`w-[50px] h-[50px] ${
-						dropDownStatus && 'bg-gradient'
-					} rounded-full shadow-right-arrow flex justify-center items-center text-[#FF2150] cursor-pointer`}
-					onClick={() => setDropDownStatus(!dropDownStatus)}
-				>
-					{dropDownStatus ? (
-						<TbChevronDown className='text-3xl text-white' />
-					) : (
-						<TbChevronRight className='text-3xl' />
-					)}
-				</div>
-			</div>
-			<div
-				className={`overflow-hidden !duration-500 ${
-					dropDownStatus
-						? 'max-h-[400px] transition-opacity delay-100'
-						: 'max-h-0 opacity-0'
-				}`}
-			>
-				<p className='w-full text-[18px] font-[400] leading-[30px] mt-4'>
-					{answer}
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={`lg:w-4/5 w-[95%] box-border transition-all border-2 border-transparent items-start mx-auto bg-white flex flex-col justify-between lg:px-10 lg:py-10 px-6 py-6 font-[500] lg:text-[22px] text-lg rounded-xl
+			${isOpen && "!border-[#FF2150]"}`}
+      onClick={() => setIsOpen((prev) => !prev)}
+    >
+      <div className="flex justify-between w-full items-center">
+        <p className="w-4/5">{question}</p>
+        <AnimatePresence initial={false} mode="wait">
+          <motion.div
+            key={isOpen ? "bottom" : "right"}
+            initial={{
+              rotate: isOpen ? -90 : 90,
+            }}
+            animate={{
+              zIndex: 1,
+              rotate: 0,
+              transition: {
+                type: "tween",
+                duration: 0.15,
+                ease: "circOut",
+              },
+            }}
+            exit={{
+              zIndex: 0,
+              rotate: isOpen ? -90 : 90,
+              transition: {
+                type: "tween",
+                duration: 0.15,
+                ease: "circIn",
+              },
+            }}
+            className={`w-[50px] h-[50px] ${
+              isOpen && "bg-gradient"
+            } rounded-full shadow-right-arrow flex justify-center items-center text-[#FF2150] cursor-pointer`}
+          >
+            {isOpen ? (
+              <TbChevronDown className="text-3xl text-white" />
+            ) : (
+              <TbChevronRight className="text-3xl" />
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+      <AnimatePresence mode="wait">
+        {isOpen && (
+          <motion.div
+            initial={{
+              height: 0,
+              opacity: 0,
+            }}
+            animate={{
+              height: "auto",
+              opacity: 1,
+              transition: {
+                height: {
+                  duration: 0.2,
+                },
+                opacity: {
+                  duration: 0.25,
+                  delay: 0.15,
+                },
+              },
+            }}
+            exit={{
+              height: 0,
+              opacity: 0,
+              transition: {
+                height: {
+                  duration: 0.2,
+                },
+                opacity: {
+                  duration: 0.25,
+                },
+              },
+            }}
+          >
+            <p className="w-full cursor-pointer text-[18px] font-[400] leading-[30px] mt-4">
+              {answer}
+            </p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
 };
 
 export default AccordiumItem;
