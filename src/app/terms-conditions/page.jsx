6d79a2e80ac2dbd262ab8505d63e1013de24f0bb
anbehindY <yanminthwin@gmail.com>
2024-02-16 21:58:05 +0630

    Add loading skeleton

--- src/app/terms-conditions/page.jsx
+++ src/app/terms-conditions/page.jsx
@@ -4,6 +4,7 @@ import MainLayout from "@/components/shared/MainLayout";
 import { endpoints } from "@/utils/endpoints";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import FooterSkeleton from "@/components/shared/FooterSkeleton";
 
 const TermsConditions = () => {
   const [content, setContent] = useState(null);
@@ -45,14 +46,7 @@ const TermsConditions = () => {
           </div>
         </div>
       ) : (
-        <div className="w-full h-full px-2 py-8 lg:px-6 lg:py-20">
-          <div className="bg-[#96969647] border-[#666666DB] border-[2px] rounded-xl w-full lg:w-11/12 mx-auto py-4 lg:py-28 text-black leading-7 flex flex-col gap-4 lg:gap-8">
-            <div
-              className={`lg:w-4/5 w-[95%] box-border h-screen animate-pulse border-2 border-transparent items-start mx-auto bg-white flex flex-col justify-between lg:px-10 lg:py-10 px-6 py-6 font-[500] lg:text-[22px] text-lg rounded-xl
-    	`}
-            ></div>
-          </div>
-        </div>
+        <FooterSkeleton />
       )}
     </MainLayout>
   );
