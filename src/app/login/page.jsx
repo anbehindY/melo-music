0ce7c5e7fac474ab5c54847bc1ec9dac6e40484a
anbehindY <yanminthwin@gmail.com>
2024-02-17 21:28:12 +0630

    Update login UI

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { GoogleSignInButton } from "@/components/AuthButtons";
-import GradientBackground from "@/components/shared/GradientBackground";
+
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,8 +12,35 @@ export default function LoginPage() {
   console.log(session);
   if (session) {
     return (
-      <GradientBackground>
-        <nav className="flex items-center justify-between flex-wrap px-6 h-[99px] bg-[#0F0F0F66] sticky top-0 w-full z-50 lg:h-[125px] lg:px-16">
+      <div className="bg-[#27282E]">
+        <div className="bg-gradient-rect h-screen bg-no-repeat bg-top bg-[length:100%_50%] flex flex-col justify-start gap-20 items-center">
+          <nav className="flex items-center justify-between flex-wrap px-6 h-[99px] sticky top-0 w-full z-50 lg:h-[120px] lg:px-16">
+            <div className="relative w-[100px] h-[40px] lg:w-[200px] lg:h-[77px] cursor-pointer">
+              <Image
+                src="/Logo.svg"
+                fill
+                alt="Melo logo"
+                style={{ objectFit: "contain" }}
+                onClick={() => router.push("/")}
+              />
+            </div>
+            <button
+              className="text-lg font-semibold cursor-pointer border-2 rounded-full px-6 py-2 text-[#FF2150] border-[#FF2150]
+            hover:bg-[#FF2150] hover:text-white hover:shadow-listen-btn"
+              onClick={() => signOut()}
+            >
+              Sign out
+            </button>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="bg-[#27282E]">
+      <div className="bg-gradient-rect h-screen bg-no-repeat bg-top bg-[length:100%_50%] flex flex-col justify-start gap-20 items-center">
+        <nav className="flex items-center justify-between flex-wrap px-6 h-[99px] sticky top-0 w-full z-50 lg:h-[120px] lg:px-16">
           <div className="relative w-[100px] h-[40px] lg:w-[200px] lg:h-[77px] cursor-pointer">
             <Image
               src="/Logo.svg"
@@ -23,43 +50,36 @@ export default function LoginPage() {
               onClick={() => router.push("/")}
             />
           </div>
-          <button
+        </nav>
+        <form className="bg-[#f5f5f5] md:w-[572px] flex flex-col justify-center items-center gap-4 rounded-lg p-8">
+          <div className="relative w-[150px] h-[150px] cursor-pointer">
+            <Image
+              src="/logo/signIn_logo.svg"
+              fill
+              alt="Melo logo"
+              style={{ objectFit: "contain" }}
+              onClick={() => router.push("/")}
+            />
+          </div>
+          <h3 className="text-[#0f1016] text-3xl font-bold">Log In to Melo</h3>
+          <small className="bg-clip-text text-transparent font-medium bg-gradient-to-r from-[#ff512f] text-[13px] to-[#dd2476]">
+            Melo to your Heart!
+          </small>
+          <div className="w-4/6">
+            <GoogleSignInButton />
+          </div>
+          {/* <button
             className="text-lg font-semibold cursor-pointer border-2 rounded-full px-6 py-2 text-[#FF2150] border-[#FF2150]
             hover:bg-[#FF2150] hover:text-white hover:shadow-listen-btn"
             onClick={() => signOut()}
           >
             Sign out
-          </button>
-        </nav>
-        <div className="w-full h-full px-2 py-8 lg:px-6 lg:py-20">
-          <div className="bg-[#96969647] border-[#666666DB] border-[2px] rounded-xl w-full lg:w-11/12 mx-auto py-4 lg:py-28 text-white leading-7 flex flex-col gap-4 lg:gap-8">
-            <div className="flex flex-col justify-start items-center gap-4 h-[calc(100vh-125px)]">
-              <p>Signed in as {session.user.email}</p>
-              <code className="text-center text-sm">{session.id_token}</code>
-            </div>
-          </div>
-        </div>
-      </GradientBackground>
-    );
-  }
-  return (
-    <GradientBackground>
-      <nav className="flex items-center justify-between flex-wrap px-6 h-[99px] bg-[#0F0F0F66] sticky top-0 w-full z-50 lg:h-[125px] lg:px-16">
-        <div className="relative w-[100px] h-[40px] lg:w-[200px] lg:h-[77px] cursor-pointer">
-          <Image
-            src="/Logo.svg"
-            fill
-            alt="Melo logo"
-            style={{ objectFit: "contain" }}
-            onClick={() => router.push("/")}
-          />
-        </div>
-      </nav>
-      <div className="h-[calc(100vh-125px)] flex justify-center pt-40">
-        <div className="w-5/6">
-          <GoogleSignInButton />
-        </div>
+          </button> */}
+        </form>
+        <small className="text-[#d3d3d3]">
+          © 2024 All Rights Reserved. Melo Music
+        </small>
       </div>
-    </GradientBackground>
+    </div>
   );
 }
